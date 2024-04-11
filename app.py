70e822b44cdcadcc942248d2c47dfc14228dd17d
xdalbel <szamosfalviabel0@gmail.com>
2024-04-11 20:30:40 +0200

    Refactor CSS styles and update manifest.json

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ app = Flask(__name__)
 
 trains = []
 
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
-email_file = 'email_list.txt'
 
 def update(filename, number):
     with open(filename, 'r') as f:
@@ -19,12 +17,12 @@ def update(filename, number):
     return lines[-number:]
 
 def load_trains():
-    with open('trains.txt', 'a', encoding='utf-8') as f:
+    with open('static/trains.txt', 'a', encoding='utf-8') as f:
         f.write(f'{closest_train};{update_time}\n')
 
 def render_onsite():
-    with open('trains.txt', 'r', encoding='utf-8') as lines:
-        last_five = update('trains.txt', 5)
+    with open('static/trains.txt', 'r', encoding='utf-8') as lines:
+        last_five = update('static/trains.txt', 5)
         for line in last_five:
             line = line.strip('\n')
             trains.append(line.split(';'))
@@ -35,53 +33,6 @@ def telegram_send(message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     myobj = {'chat_id': 'YOUR_CHAT_ID', 'parse_mode': 'MARKDOWN', 'text': f"{message} \n"}
     x = requests.post(url, json=myobj)
-
-def send_email(subject, body, receiver_emails):
-    sender_email = 'railsense.automated@gmail.com'
-    smtp_server = 'smtp.gmail.com'
-    smtp_port = 587
-    smtp_username = 'railsense.automated@gmail.com'
-    smtp_password = 'zioxopmsmofaouen'  # It's forbidden to change this password
-    message = f"Subject: {subject}\n\n{body}"
-    message = message.encode('utf-8')
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(smtp_username, smtp_password)
-        server.sendmail(sender_email, receiver_emails, message)
-
-def save_email(email):
-    valid_mails = [
-        '@gmail.com',
-        '@yahoo.com',
-        '@outlook.com',
-        '@hotmail.com',
-        '@aol.com',
-        '@icloud.com',
-        '@mail.com',
-        '@zoho.com',
-        '@protonmail.com',
-        '@yandex.com',
-        '@live.com',
-        '@inbox.com',
-        '@fastmail.com',
-        '@rocketmail.com',
-        '@tutanota.com',
-        '@upcmail.hu',
-    ]
-    for i in valid_mails:
-        if i in email:
-            with open(os.path.join(current_dir, 'static', email_file), 'a') as file:
-                file.write(email + '\n')
-
-def load_emails():
-    emails = []
-    if os.path.exists(os.path.join(current_dir, 'static', email_file)):
-        with open(os.path.join(current_dir, 'static', email_file), 'r') as file:
-            for line in file:
-                email = line.strip()
-                if email:
-                    emails.append(email)
-    return emails
 
 def calculate_time_to_reach_coordinate(start_lat, start_lon, finish_lat, finish_lon, speed):
     lat1 = math.radians(start_lat)
@@ -131,12 +82,6 @@ def index():
                         time_to_reach = calculate_time_to_reach_coordinate(lat, lon, target_lat, target_lon, speed)
                         close_trains.append((item["direction"], time_to_reach))
 
-        if request.method == 'POST':
-            email = request.form.get('email')
-            if email:
-                if email not in load_emails():
-                    save_email(email)
-
         if close_trains:
             with open('static/ek.txt', 'r') as f:
                 ek = f.read()
@@ -155,21 +100,20 @@ def index():
                     f"*Az átkelés veszélyes!*\n\nSzerelvény érkezik: *{closest_train[0]}* irányban, \n*{closest_train[1]}* másodpercen belül.")
             else:
                 pass
-            # email_list = load_emails()
-            # send_email(subject, body, email_list) #ha ezt a kommentet kiszeded az oldal minden megnyitaskor emailt kuildd ha van vonat. fontos ha nincs megnyitva akkor nem kuldd
-            return render_template('respon.html', message=message, directions=[closest_train[0]], result_status=result_status, update_time=update_time, trains=trains, manifest_url=manifest_url)
+            
+            return render_template('index.html', message=message, directions=[closest_train[0]], result_status=result_status, update_time=update_time, trains=trains, manifest_url=manifest_url)
 
         else:
             render_onsite()
             with open('static/ek.txt', 'w+') as f:
                 f.write('0')
             result_status = 2
-            return render_template('respon.html',
+            return render_template('index.html',
                                    message="Óvatosan és magabiztosan közlekedj! Nézz körül minden esetben! ",
                                    result_status=result_status, update_time=update_time, manifest_url=manifest_url)
     else:
         result_status = 3
-        return render_template('respon.html', message=f"Error occurred: {response.status_code}",
+        return render_template('index.html', message=f"Error occurred: {response.status_code}",
                                result_status=result_status, update_time=update_time, manifest_url=manifest_url)
 
 
